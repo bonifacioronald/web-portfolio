16adb55682ca93131f5229619c45bd084d641895
bonifacioronald <bonifacioronald99@gmail.com>
2024-04-29 23:42:56 +0100

    Fix self info content

--- src/edu-exp/edu-exp-section.jsx
+++ src/edu-exp/edu-exp-section.jsx
@@ -1,4 +1,4 @@
-import React, { useDebugValue } from "react";
+import React from "react";
 import "./edu-exp.css";
 import 'bootstrap/dist/css/bootstrap.css';
 
--- src/self-info/self-info-section.jsx
+++ src/self-info/self-info-section.jsx
@@ -7,8 +7,8 @@ function SelfInfoSection() {
     
   <div className="top-section">
             <h1 className="name roboto-bold mb-3">Bonifacio Ronald</h1>
-            <h2 className="role roboto-regular mb-5">Software Developer & Blockchain Engineer</h2>
-            <h3 className="description roboto-regular mt-5">Final year Bachelor of Computer Science (Hons.) student with a strong background in software development and particular interest in Web3 technologies.</h3>
+            <h2 className="role roboto-regular mb-5">Software & Blockchain Developer</h2>
+            <h3 className="description roboto-regular mt-5">Hi, there! I'm a final-year student based in Malaysia and Indonesia, with a solid foundation in website and mobile application development. I'm particularly passionate about exploring the potential of Web3 technologies. Currently, I'm on the lookout for remote opportunities that align with my professional growth while allowing me to stay focused on my academic journey. <a className="cv-link"href="https://drive.google.com/file/d/1hRrLCjVwuXiA0k2MP9Bed2iYoM6Guu52/view?usp=sharing">Download my full resume here</a></h3>
   </div>
   );
 }
