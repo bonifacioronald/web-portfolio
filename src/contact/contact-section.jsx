fc0ca4c045b15d9c6c101b765c9dfa387312c65e
bonifacioronald <bonifacioronald99@gmail.com>
2024-04-29 20:39:58 +0100

    Add icons and styling to contact section

--- src/contact/contact-section.jsx
+++ src/contact/contact-section.jsx
@@ -8,9 +8,9 @@ function ContactSection() {
         <div>
             <img className="circle-avatar" src={require('../assets/images/profile-pic.png')} alt="profile pic" />
         </div>
-        <div><a href="https://www.linkedin.com/in/bonifacio-ronald/">LinkedIn</a></div>
-        <div><a href="https://github.com/bonifacioronald">GitHub</a></div>
-        <div><a href="https://mailto:bonifacioronald.dev@gmail.com">Email</a></div>
+        <div><a href="https://www.linkedin.com/in/bonifacio-ronald/"><i class="fa fa-linkedin-square"></i>&ensp;LinkedIn</a></div>
+        <div><a href="https://github.com/bonifacioronald"><i class="fa fa-github"></i>&ensp;GitHub</a></div>
+        <div><a href="https://mailto:bonifacioronald.dev@gmail.com"><i class="fa fa-envelope"></i>&ensp;Email</a></div>
     </div>
     );
 }
