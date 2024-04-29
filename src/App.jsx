157a4a86bc4b4af61a3297023af56dafa9242df0
bonifacioronald <bonifacioronald99@gmail.com>
2024-04-29 20:47:31 +0100

    Add styling to copyright footer text

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,14 @@ function App() {
     <div className="App">
       <div className="layout-container">
         <div className='left-section'>
-          <div><SelfInfoSection/></div>
+          <div className='mb-5'><SelfInfoSection/></div>
           <div><ContactSection/></div>
           </div>
         <div className='right-section'>
           <ProjectSection/>
           <ProjectSection/>
           <ProjectSection/>
-          <h5 className='mt-5'>Made with React.js by Bonifacio Ronald @ 2024<br/>Design inspired by Nicolas Meuzard</h5>
+          <h5 className='copyright mt-5'>Made with React.js by Bonifacio Ronald @ 2024<br/>Design inspired by <a className='copyright-link' href="https://dribbble.com/NicolasMzrd">Nicolas Meuzard</a></h5>
         </div>
       </div>
     </div>
---
+++ src/edu-exp/edu-exp-section.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./edu-exp.css";
+import 'bootstrap/dist/css/bootstrap.css';
+
+function EduExpSection() {
+    return (
+        <div>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+            <h1>TEST</h1>
+        </div>
+    );
+}
+
+export default EduExpSection;
