1c6981c4e4229be1690141e5be86d9fa9e40a5b8
bonifacioronald <bonifacioronald99@gmail.com>
2024-04-30 16:42:31 +0100

    Change facicon and awards index styling

--- src/self-info/self-info-section.jsx
+++ src/self-info/self-info-section.jsx
@@ -8,7 +8,7 @@ function SelfInfoSection() {
   <div className="top-section">
             <h1 className="name roboto-bold mb-3">Bonifacio Ronald</h1>
             <h2 className="role roboto-regular mb-5">Software & Blockchain Developer</h2>
-            <h3 className="description roboto-regular mt-5">Hi, there! I'm an aspiring mobile and web app developer based in Malaysia and Indonesia, with a keen interest in Web3 technologies. Currently, I'm seeking remote opportunities for my professional growth while maintaining my academic focus. <a className="cv-link"href="https://drive.google.com/file/d/1hRrLCjVwuXiA0k2MP9Bed2iYoM6Guu52/view?usp=sharing">View my resume</a></h3>
+            <h3 className="description roboto-regular mt-5">Hi, there! I'm an aspiring mobile and web app developer based in Malaysia and Indonesia with a keen interest in Web3 technologies. Currently, I'm seeking remote opportunities for my professional growth while maintaining my academic focus. <a className="cv-link"href="https://drive.google.com/file/d/1hRrLCjVwuXiA0k2MP9Bed2iYoM6Guu52/view?usp=sharing">View my resume</a></h3>
   </div>
   );
 }
